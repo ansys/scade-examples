29e34f2f40312416c006fd9c87c0eb511817134d
Ludovic Oddos (Ansys) <34745062+ansloddos@users.noreply.github.com>
2026-03-24 13:42:19 +0100

    maint: upgrade Python UA example 2026R1 (#59)
    * upgarde 2026R1

--- models/2025-08-25-a661-python-ua/start.bat
+++ models/2025-08-25-a661-python-ua/start.bat
@@ -5,7 +5,7 @@
 :: ----------------------------
 :setup_env_variables
 :: Change this variable to your local SCADE installation path if required
-set SCADE_INSTALL=C:\Program Files\ANSYS Inc\v252\SCADE
+set SCADE_INSTALL=C:\Program Files\ANSYS Inc\v261\SCADE
 set PYTHONPATH=%SCADE_INSTALL%\SCADE A661\PythonLib;%SCADE_INSTALL%\SCADE\APIs\Python\lib
 set VENV=.venv
 echo Using SCADE installation: %SCADE_INSTALL%
@@ -19,7 +19,7 @@ echo Using Python venv: %VENV%
 :build_df
 if not exist "graphics\DF\UA_1.bin" (
     echo Building the binary DF, this may take a while...
-    "%SCADE_INSTALL%\SCADE A661\DFGEN68\bin\dfgen.exe" ^
+    "%SCADE_INSTALL%\SCADE A661\DFGEN681\bin\dfgen.exe" ^
         -n "%SCADE_INSTALL%\SCADE Display\config\a661_description\a661.xml" ^
         -outdir "graphics\DF" -o "UA_1" "graphics\UADF.sgfx"
     if ERRORLEVEL 1 (echo Failed to build binary DF, exiting && goto :end)
@@ -31,7 +31,7 @@ if not exist "graphics\DF\UA_1.bin" (
 :setup_venv
 if not exist %VENV% (
     echo Creating Python virtual environment, this may take a while...
-    "%SCADE_INSTALL%\contrib\Python310\python.exe" -m venv %VENV%
+    "%SCADE_INSTALL%\contrib\Python312\python.exe" -m venv %VENV%
     call .\%VENV%\Scripts\activate.bat
 ) else (
     echo Activating Python virtual environment: %VENV%
